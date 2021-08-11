fc878448016605f93b98bbb5cf1253bd80aee2b3
RISHABH KUMAR <62342470+luharuka@users.noreply.github.com>
2021-08-11 18:27:29 +0530

    day 2 assignment

---
+++ Day2.java
@@ -0,0 +1,9 @@
+public class Day2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        sum = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+        System.out.println("Command Line arg:"+sum);
+        
+        
+    }
+}
